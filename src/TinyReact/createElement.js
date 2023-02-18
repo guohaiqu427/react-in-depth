fd976577f8125fc9be33504d3aa9a9b99c378c83
Vincent <quvincent23@gmail.com>
2023-02-18 13:26:57 +0100

    TinyReact.createElement function done, with notes

--- src/TinyReact/createElement.js
+++ src/TinyReact/createElement.js
@@ -1,15 +1,18 @@
 export default function createElement (type, props, ...children ){
-    const childElements = [].concat(...children).map(child=>{
-        if(child instanceof Object) {
-            return child
-        }else{
-            return createElement("text", {textContent:child})
+    const childElements = [].concat(...children).reduce((result, child)=>{
+        if(child !== false && child!== true && child!== null){
+            if(child instanceof Object) {
+                result.push(child) 
+            }else{
+                result.push(createElement("text", {textContent:child}))  
+            }
         }
-    })
+        return result
+    },[])
 
     return {
         type,
-        props,
+        props: Object.assign({children:childElements},props),
         children:childElements
     }
 }
