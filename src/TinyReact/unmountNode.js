e37e966512010190b02d17b20e88a7538dfe8abb
Guohai Qu <vincent@Guohais-MacBook-Pro.local>
2023-02-20 22:05:33 +0100

    remove component

--- src/TinyReact/unmountNode.js
+++ src/TinyReact/unmountNode.js
@@ -1,3 +1,34 @@
 export default function unmountNode(node) {
-    node.remove()
+    //1. unmount text node 
+    const virtualDOM = node._virtualDOM
+    if(virtualDOM.type === "text"){
+        node.remove()
+        return 
+    }
+    // 2. unmount component 
+    // 2a. hook: component will unmount
+    let component = virtualDOM.component
+   if(component) {
+    component.componentWillUnmount()
+   }
+   //3. remove ref 
+   if(virtualDOM.props && virtualDOM.props.ref){
+        virtualDOM.props.ref(null)
+   }
+   //4. remove eventlistenr
+   Object.keys(virtualDOM.props).forEach(propName => {
+    if(propName.slice(0,2) === "on"){
+        const eventName = propName.toLocaleLowerCase().slice(2)
+        const eventHandler = virtualDOM.props[propName]
+        node.removeEventListener(eventName, eventHandler)
+    }
+   })
+   //5. remove child nodes
+   if(node.childNodes.length > 0){
+    for(let i=0; i<node.childNodes.length; i++){
+        unmountNode(node.childNodes[i])
+        i--
+    }
+   }
+   node.remove()
 }
